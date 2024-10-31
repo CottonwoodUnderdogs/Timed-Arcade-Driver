edc359c5a4c66f1677b612bddea6876dbd8649b5
CottonwoodUnderdogs <roboticscottonwood66@gmail.com>
2024-10-31 16:39:40 -0600

    initial commit

---
+++ src/main/java/frc/robot/climber.java
@@ -0,0 +1,78 @@
+
+    package frc.robot;
+
+import java.util.Set;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+ public class climber {
+        private  CANSparkMax m_upMotor;
+
+
+//if(m_upMotor.getRawButton)
+//       private XboxController controller;
+
+//       //Constructor
+//       public climber(CANSparkMax A, Joystick B, Joystick C) {
+//         m_upMotor = A;
+      
+
+// public void Joystick_up_down() {
+
+//     if(m_leftStick.getRawButton(4)==true && m_rightStick.getRawButton(5)==false  ) {
+//         m_upMotor.set(0.8);  }
+   
+//     if(m_leftStick.getRawButton(4)==false && m_rightStick.getRawButton(5)==false  ) {
+//         m_upMotor.set(0);  }
+
+//     if(m_leftStick.getRawButton(4)==false && m_rightStick.getRawButton(5)==true  ) {
+//        m_upMotor.set(-0.8);  }
+
+//     if(m_leftStick.getRawButton(4)==true && m_rightStick.getRawButton(5)==true  ) {
+//         m_upMotor.set(0);  }
+//     }
+
+//     
+
+
+/* 
+  
+ if(m_upMotor.getRawButton(5)==true && left==false){
+  m_upMotor.set(0.6);
+
+  } else {
+   
+    m_upMotor.set(0);
+  }
+  if(m_leftStick.getRawButton(5)==true){
+  m_upMotor.set(-0.6);
+
+  } else {
+    m_upMotor.set(0);
+  }
+
+}
+*/
+
+
+
+
+
+  
+    
+
+
+
+
+
+
+
+
+   
+
+
+    
+}
+   
